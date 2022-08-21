658723c1384329bf011b6ee0e94636db11a551e6
Isaac <ifimbres@uabc.edu.mx>
2022-08-21 14:39:47 -0700

    Agregar archivos de proyecto.

---
+++ Moviely/Controllers/MoviesController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moviely.Models;
+
+namespace Moviely.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public IActionResult Random()
+        {
+            var movie = new Movie() { Title = "Shrek" };
+
+            return View(movie);
+        }
+    }
+}
