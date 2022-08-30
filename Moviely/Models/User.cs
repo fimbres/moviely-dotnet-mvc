eb301ea7825d0dc9800b5ec693b1960f854ef5e3
Isaac <ifimbres@uabc.edu.mx>
2022-08-29 20:52:01 -0700

    new login logic

---
+++ Moviely/Models/User.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Moviely.Models
+{
+    public class User : IdentityUser
+    {
+        public string Username { get; set; }
+        public string password { get; set; }
+    }
+}
